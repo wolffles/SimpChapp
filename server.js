716e60096d5178b851bb6711201e35c9c26918b8
Wolffles <wolfgang.truong@gmail.com>
2020-06-05 10:28:21 -0700

    nothing happened

--- server.js
+++ server.js
@@ -18,61 +18,130 @@ app.use(express.static(path.join(__dirname, 'public'),{
 // Chatroom
 
 var numUsers = 0;
+var normalUsers = 0;
+var otherUsers = 0;
+
+
+// when this app sends a message it sends it to every room, need to change that
+const roomEmit = (url, socket, data) => {
+  // we tell the client to execute 'new message'
+  if (url.includes("nothinghere")){
+    socket.to('nothinghere').emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  }else{
+    socket.to('normal').emit('new message', {
+    username: socket.username,
+    message: data
+    });
+  }
+}
 
 io.on('connection', (socket) => {
+  let url = socket.request.headers.referer
+  if (url.includes("nothinghere")){
+    console.log("joined nothinghere")
+    socket.join('nothinghere')
+  }else {
+    console.log('joined normal')
+    socket.join('normal')
+  }
   var addedUser = false;
 
   // when the client emits 'new message', this listens and executes
   socket.on('new message', (data) => {
     // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
-    });
+    // if (url.includes("nothinghere")){
+    //   socket.to('nothinghere').emit('new message', {
+    //     username: socket.username,
+    //     message: data
+    //   });
+    // }else{
+    //   socket.broadcast.emit('new message', {
+    //   username: socket.username,
+    //   message: data
+    //   });
+    // }
+    roomEmit(url,socket,data)
   });
 
   // when the client emits 'add user', this listens and executes
   socket.on('add user', (username) => {
     if (addedUser) return;
-
-    // we store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
+    socket.username = username
     addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
+    // we store the username in the socket session for this client
     // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
+    if (url.includes("nothinghere")){
+      ++otherUsers
+      socket.emit('login', {
+        numUsers: otherUsers
+      })
+      socket.to('nothinghere').emit('user joined', {
+        username: socket.username,
+        numUsers: otherUsers
+      })
+    }else{
+      ++normalUsers
+      socket.emit('login', {
+        numUsers: normalUsers
+      });
+      socket.to('normal').emit('user joined', {
+        username: socket.username,
+        numUsers: normalUsers
+      });
+    }
   });
 
   // when the client emits 'typing', we broadcast it to others
   socket.on('typing', () => {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
+    if (url.includes("nothinghere")){
+      socket.to('nothinghere').emit('typing', {
+        username:socket.username,
+      })
+    }else{
+      socket.to('normal').emit('typing', {
+        username: socket.username,
+      });
+    }
   });
 
   // when the client emits 'stop typing', we broadcast it to others
   socket.on('stop typing', () => {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
-    });
+    // socket.broadcast.emit('stop typing', {
+    //   username: socket.username
+    // });
+    if (url.includes("nothinghere")){
+      socket.to('nothinghere').emit('stop typing', {
+        username:socket.username,
+      })
+    }else{
+      socket.to('normal').emit('stop typing', {
+        username: socket.username,
+      });
+    }
   });
 
   // when the user disconnects.. perform this
   socket.on('disconnect', () => {
     if (addedUser) {
-      --numUsers;
-
       // echo globally that this client has left
-      socket.broadcast.emit('user left', {
+      // socket.broadcast.emit('user left', {
+      //   username: socket.username,
+      //   numUsers: numUsers
+      // });
+       if (url.includes("nothinghere")){
+        --otherUsers;
+        socket.to('nothinghere').emit('user left', {
+          username:socket.username,
+        })
+    }else{
+      --normalUsers;
+      socket.to('normal').emit('user left', {
         username: socket.username,
-        numUsers: numUsers
       });
+    }
     }
   });
 });
