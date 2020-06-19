19da2a1d69625e892ac8caeb03e3ad0ff24ea248
Wolffles <wolfgang.truong@gmail.com>
2020-06-19 11:39:25 -0700

    app isn't starting

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public'),{
   extensions: ['html']
 }));
 
+
 app.post("/swsubscription", subscriptionHandler.handlePushNotificationSubscription);
 app.get("/subscription/allothers/:id", subscriptionHandler.sendAllOthersPushNotification);
 app.get("/subscription/:id", subscriptionHandler.sendPushNotification);
